13d5d039dd32a346f48a53fd30817e88752af1f2
SoulzzDev <aatuheinanen@gmail.com>
2021-04-27 16:45:23 +0300

    ....

---
+++ Scenes/MainFamilyTree.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+func _on_Button2_pressed():
+	print("works!")
+
+func _on_Button3_pressed():
+	pass # Replace with function body.
+
+func _on_Button4_pressed():
+	pass # Replace with function body.
+
+func _on_Button5_pressed():
+	pass # Replace with function body.
+
+func _on_Button7_pressed():
+	pass # Replace with function body.
+
+func _on_Button8_pressed():
+	pass # Replace with function body.
