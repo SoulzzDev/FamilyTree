47f3e9b7a98e4b449463b38910de02c7d3facf0a
SoulzzDev <aatuheinanen@gmail.com>
2021-04-27 13:20:23 +0300

    ...

---
+++ Autoloads/Variables.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var renew_pressed = false
--- Photo.gd
+++ Photo.gd
@@ -2,11 +2,13 @@ extends Control
 
 onready var fileDialog = $FileDialog
 onready var Textures = $TextureRect
+onready var FileButton = $Button
+onready var Frame = $Frame
+
+var texture_placed = false
 
 func _on_Button_pressed():
 	fileDialog.popup()
-
-
 
 
 func _on_FileDialog_file_selected(path):
@@ -15,3 +17,10 @@ func _on_FileDialog_file_selected(path):
 	image.load(path)
 	image_texture.create_from_image(image)
 	Textures.texture = image_texture
+	texture_placed = true
+	print(image.get_width())
+	print(image.get_height())
+
+func _physics_process(delta):
+	if texture_placed == true:
+		Frame.visible = false
